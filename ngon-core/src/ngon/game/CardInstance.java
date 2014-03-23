02021f3d71ab8b836608afda53da42ad1f063f1a
stuyvenberg <stuyvenberg@wisc.edu>
2014-03-23 17:35:16 -0500

    Initial commit
    Adds the Card class and some other stuffs.

---
+++ ngon-core/src/ngon/game/CardInstance.java
@@ -0,0 +1,15 @@
+package ngon.game;
+
+public class CardInstance
+{
+	public Card type;
+	
+	public ZoneInstance zone;
+	public double x, y, angle;
+	public boolean flipped;
+	
+	public CardInstance(Card type, ZoneInstance zone, double x, double y, double angle, boolean flipped)
+	{
+		
+	}
+}
